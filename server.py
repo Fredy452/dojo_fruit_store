3e17b284592b074a004dcdb12460b92e5c78a57d
Fred452 <fredyarce452@gmail.com>
2023-08-06 10:55:58 -0400

    Creacion

--- server.py
+++ server.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, session
+
 app = Flask(__name__)  
+
+
+# Configurar Secret Key para poder almacenar en cache
+app.secret_key = "supersecret"
 
 @app.route('/')         
 def index():
@@ -7,7 +12,25 @@ def index():
 
 @app.route('/checkout', methods=['POST'])         
 def checkout():
-    print(request.form)
+
+    #En vez de enviar los formularios por parametro vamos a almacenar en session para
+    #Recuperar en el checkout
+
+    # Datos de las frutas
+    session["strawberry"] = request.form["strawberry"]
+    session["raspberry"] = request.form["raspberry"]
+    session["apple"] = request.form["apple"]
+
+    # Datos del usuario
+    session["first_name"] = request.form["first_name"]
+    session["last_name"] = request.form["last_name"]
+    session["student_id"] = request.form["student_id"]
+
+    #Cobrando a cliente
+    total_amount = int(session['strawberry']) + int(session['raspberry']) + int(session['apple'])  # noqa: E501
+    print(f"Cobrando a {session['first_name']} por total {total_amount} frutas")
+
+
     return render_template("checkout.html")
 
 @app.route('/fruits')         
